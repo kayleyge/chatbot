4ec36fc4c25f4d1bb77e2e6583ac63c8ee9a2fc3
kayleyge <geka@DESKTOP-1LO2M29.localdomain>
2024-03-18 20:59:58 -0400

    first commit

---
+++ baymin.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+
+client = OpenAI()
+
+response = client.audio.speech.create(
+    model="tts-1",
+    voice="alloy",
+    input="Hello world! This is a streaming test.",
+)
+
+response.stream_to_file("output.mp3")
+
+
+#Speech To text
+from openai import OpenAI
+client = OpenAI()
+
+audio_file= open("/path/to/file/audio.mp3", "rb")
+transcription = client.audio.transcriptions.create(
+  model="whisper-1", 
+  file=audio_file
+)
+print(transcription.text)
+
+
